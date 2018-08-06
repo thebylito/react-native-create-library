c7b321a111b797c89a88279689010a693f823c38
Welington da Silva Martins <welington.martins@sanesul.ms.gov.br>
2018-08-06 10:44:51 -0400

    view

--- templates/android.js
+++ templates/android.js
@@ -117,10 +117,6 @@ afterEvaluate { project ->
             configureReactNativePom pom
         }
     }
-
-    task clean(type: Delete) {
-        delete rootProject.buildDir
-    }
 }
   `,
 }, {
@@ -171,6 +167,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class ${name}Package implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
@@ -184,7 +181,89 @@ public class ${name}Package implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Arrays.<ViewManager>asList(
+            new ${packageIdentifier.split('.').join('/')}/${name}Manager()
+    );
+    }
+}`,
+}, {
+    name: ({ packageIdentifier, name }) =>
+        `${platform}/src/main/java/${packageIdentifier.split('.').join('/')}/${name}Manager.java`,
+    content: ({ packageIdentifier, name }) => `
+package ${packageIdentifier};
+
+import android.app.Activity;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.View;
+
+
+import com.facebook.infer.annotation.Assertions;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+public class ${name}Manager extends SimpleViewManager<View> {
+    private ThemedReactContext mContext = null;
+    private Activity mActivity = null;
+
+    public static final String REACT_CLASS = " ${name}Manager";
+    public static final int NAME = 1;
+    public static final int NAME2 = 2;
+
+    @Override
+    public String getName() {
+        // Tell React the name of the module
+        // https://facebook.github.io/react-native/docs/native-components-android.html#1-create-the-viewmanager-subclass
+        return REACT_CLASS;
+    }
+    @Override
+    public View createViewInstance(ThemedReactContext reactContext) {
+        mContext = reactContext;
+        mActivity = mContext.getCurrentActivity();
+        return new View(mContext);
+    }
+
+    @ReactProp(name = "prop")
+    public void methodProp(View view) {
+
+    }
+
+    @Override
+    public Map<String,Integer> getCommandsMap() {
+        Log.d("React"," View manager getCommandsMap:");
+        return MapBuilder.of(
+                "name",
+                NAME,
+                "name2",
+                NAME2);
+    }
+
+    @Override
+    public void receiveCommand(
+            View view,
+            int commandType,
+            @Nullable ReadableArray args) {
+        Assertions.assertNotNull(view);
+        Assertions.assertNotNull(args);
+        switch (commandType) {
+            case NAME: {
+                // action()
+                return;
+            }
+            case NAME2: {
+                // action()
+                return;
+            }
+
+            default:
+                throw new IllegalArgumentException(String.format(
+                        "Unsupported command %d received by %s.",
+                        commandType,
+                        getClass().getSimpleName()));
+        }
     }
 }`,
 }, {
